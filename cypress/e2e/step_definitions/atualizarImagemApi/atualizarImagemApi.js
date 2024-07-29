42bac2d8c9eb71732d3161c98aeea4162ad6189e
renan7k <renan7k@gmail.com>
2024-07-29 09:46:29 -0300

    AtualizandoFeatures

--- cypress/e2e/step_definitions/atualizarImagemApi/atualizarImagemApi.js
+++ cypress/e2e/step_definitions/atualizarImagemApi/atualizarImagemApi.js
@@ -11,3 +11,13 @@ And("o produto irá apresentar a nova imagem", () => {
 })
 And("o response irá apresenta o id da imagem atualizado", () => {  
 })
+
+//EXERCÍCIO NÃO REALIZADO POIS LINK COM A DOCUMENTAÇÃO DA API ESTAVA FORA, E PRECISÁVAMOS ENTENDER ALGUNS DOS PARÂMETROS ENVIADOS
+
+//{
+//    "userId": 261948927,
+//   "productsInCart": [],
+//    "sessionId": "758C7A60BD06BA64C06FB29ABD242219"
+//}
+//https://www.advantageonlineshopping.com/catalog/api/v1/product/image/261948927/{source}/{color}
+//https://www.advantageonlineshopping.com/api/docs/
